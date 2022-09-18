a04cfb46b1496916cc18879901a5d95d4f396547
Aleš Vonka <alesvonka@gmail.com>
2022-09-18 20:30:10 +0200

    Inset, update item

--- app/Components/BrandGridControl.php
+++ app/Components/BrandGridControl.php
@@ -9,10 +9,15 @@ use Nette;
 use Nette\Application\UI\Control;
 use App\Model\BrandManager;
 use Nette\Application\Attributes\Persistent;
+use Nette\Application\UI\Form;
+use Nette\Database\UniqueConstraintViolationException;
+use Nette\Utils\ArrayHash;
 use Nette\Utils\Strings;
 
 class BrandGridControl extends Control
 {
+
+    use Nette\SmartObject;
 
     #[Persistent]
     public $page = 1;
@@ -23,11 +28,65 @@ class BrandGridControl extends Control
     #[Persistent]
     public $sort = 'asc';
 
+    #[Persistent]
+    public $id;
+    public $item;
+
     public function __construct(
         private BrandManager $brandManager,
     ) {
     }
 
+    public function createComponentBrandForm(): Form
+    {
+        $form = new Form();
+        $form->getElementPrototype()->class('ajax');
+        $form->addHidden('id', null)
+            ->setNullable();
+        $form->addText('name', 'Název značky')
+            ->setRequired('Vyplňte prosím toto pole!');
+
+        $this->item = $this->brandManager->items()->get($this->id);
+
+        if ($this->item) {
+            $form['id']->value = $this->item->id;
+            $form['name']->value = $this->item->name;
+
+            $form->addSubmit('submit', 'Editovat')
+                ->setHtmlAttribute('class', 'modal-close');
+        } else {
+            $form->addSubmit('submit', 'Uložit')
+                ->setHtmlAttribute('class', 'modal-close');
+        }
+
+        $form->onSuccess[] = [$this, 'brandFormSuccess'];
+
+        return $form;
+    }
+    public function brandFormSuccess(Form $form, ArrayHash $values): void
+    {
+        if ($this->item) {
+            try {
+                $this->item->update([
+                    'name' => $values->name,
+                ]);
+            } catch (UniqueConstraintViolationException $e) {
+                $form->addError($values->name . ' už existuje. Zadejte jiný název značky.');
+            }
+        } else {
+            try {
+                $this->brandManager->items()->insert($values);
+            } catch (UniqueConstraintViolationException $e) {
+                $form->addError($values->name . ' už existuje. Zadejte jiný název značky.');
+            }
+        }
+
+        $form->reset();
+
+        $this->presenter->payload->postGet = true;
+        $this->presenter->payload->url = $this->link('this');
+        $this->redrawControl('grid');
+    }
 
     public function handleSort($sort)
     {
@@ -42,21 +101,16 @@ class BrandGridControl extends Control
 
     public function handleAdd()
     {
-        bdump(__FUNCTION__);
-        if ($this->presenter->isAjax()) {
-            $this->redrawControl('grid');
-        }
+        $this->presenter->payload->postGet = true;
+        $this->presenter->payload->url = $this->link('this');
+        $this->redrawControl('brandForm');
     }
 
     public function handleEdit($id)
     {
         if ($id) {
-            bdump(__FUNCTION__);
-            bdump($id);
-
-            if ($this->presenter->isAjax()) {
-                $this->redrawControl('grid');
-            }
+            $this->id = $id;
+            $this->redrawControl('brandForm');
         }
     }
 
@@ -74,6 +128,7 @@ class BrandGridControl extends Control
 
     public function handleP($page, $parePage)
     {
+        $this->id = null;
         $this->page = $page;
         $this->parePage = $parePage;
         if ($this->presenter->isAjax()) {
--- www/js/main.js
+++ www/js/main.js
@@ -1,17 +1,5 @@
 //Inicializace Naja pro ajaxove pozadavky
 naja.initialize();
 
-// Zobraz a skryj ajaxem modal pokud to Materializecss umi, kdyz tak smazu.
-naja.addEventListener('success', event => {
-    if (typeof event.detail.payload.hideModal !== 'undefined') {
-        $('#modal').modal('hide')
-    }
-    if (typeof event.detail.payload.showModal !== 'undefined') {
-        $('#modal').modal('show')
-    }
-});
-
-
-
 // Materializecss javasript
 M.AutoInit();
